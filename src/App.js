6c1a6673e79f001f8ef0972550cd8f522538035b
jackhsien2002 <jackhsien2002@gmail.com>
2020-01-02 20:36:52 +0800

    setup hybrid django-react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+      <div>
+      <button type="button" onClick={this.onClick}>Send GET /products </button>
+      </div>
+      );
+  }
+
+  onClick(ev) {
+      console.log("Sending a GET API Call !!!");
+      axios.get('http://127.0.0.1:8000/products').then(response => {
+        console.log(response.json())
+      }).then(response => {
+        console.log(JSON.stringify(response))
+      })
+  }
+
 }
 
 export default App;
