c52bed96db94c1b6c0c9efa64a2889a972685f3e
jackhsien2002 <jackhsien2002@gmail.com>
2020-01-06 23:00:00 +0800

    fix image text overlap when resizing

--- src/Me/Me.js
+++ src/Me/Me.js
@@ -1,24 +1,25 @@
 import React, {Component} from "react";
-import {Col, Row, Container, Image} from 'react-bootstrap';
+import {Col, Row, Container, Image, Card } from 'react-bootstrap';
 import MyPicture from './personalPicture.jpg'
 
 function Me(props) {
 
     return(
+        <div>
+
         <Container>
-            <Row>
-                <Col xs={{ span: 3, offset: 0.5 }} >
-                    <Image src={MyPicture} style={{height: "300px", width: "240"}}roundedCircle />
+            <Row >
+                <Col xs={9} md={4}>
+                    <Image  src={MyPicture} style={{height: "200x", width: "160"}}  fluid roundedCircle />
                 </Col>
-                <Col xs={{ span: 6, offset: 0.5 }} style={{ marginTop: '100px', fontFamily: "STHeiti"}}>
-                    <h1>你好，我是謝孟勳(Jack)</h1>
-                    <p>
-                        在這裡，你可以找到我個人的Project<br></br>
-                        請點擊上方的Project按鈕查看更多        
-                    </p>
+                <Col xs={9} md={4} style={{ marginTop: '100px', fontFamily: "STHeiti"}}>
+                        <h1>你好!我是Jack</h1>
+                            在這裡，你可以找到我個人的Project<br></br>
+                            請點擊上方的Project按鈕查看更多
                 </Col>
             </Row>
         </Container>
+        </div>
     );
 }
 
